0e2280cbe6156aa583c5d80a2006c9f126bc1d2b
lindamukami <lindamukami32@gmail.com>
2022-05-18 10:17:54 +0000

    added main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <limits.h>
+#include <string.h>
+
+#endif
